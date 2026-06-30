c47cea4edee058c1b07757200f5077606495aecd
Tanya <tnsrg0@gmail.com>
2026-06-30 16:33:05 +0700

    completed 4/7 tasks the lesson 2

---
+++ 02_lesson/lesson_2_task_3.py
@@ -0,0 +1,9 @@
+import math
+def square(side):
+    if side % 1 != 0:
+         print(math.ceil(side) * math.ceil(side))
+    else:
+         print(side * side)
+
+    
+square(4.6)
---
+++ 02_lesson/lesson_2_task_4.py
@@ -0,0 +1,13 @@
+def fizz_buzz(n):
+    for f_b in range(1, n):
+        if f_b % 3 == 0 and f_b % 5 == 0:
+            print('FizzBuzz')
+        elif f_b % 5 == 0:
+            print('Buzz')
+        elif f_b % 3 == 0:
+            print('Fizz')
+        else:
+            print(f_b)
+    
+
+fizz_buzz(80)
